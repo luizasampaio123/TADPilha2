ec1f0a2b32fe7d9309d67aa5ef6c8da1700c62ea
luizasampaio123 <luiza.sampaio.89@gmail.com>
2024-06-07 16:27:58 -0300

    Adicionar arquivos de projeto.

---
+++ TADPilha2/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPilha2
+{
+    internal class TADPilha
+    {
+        private int codigo;
+        private string descricao;
+        private ArrayList lista;
+        private int capacidade;
+
+
+        public TADPilha(int codigo, string descricao, int capacidade)
+        {
+            this.codigo = codigo;
+            this.descricao = descricao;
+            this.lista = new ArrayList();
+            this.capacidade = capacidade;
+        }
+        public void empilhar(int chave)
+        {
+            if(capacidade>lista.Count)
+            {
+                lista.Add(chave);
+            }
+            else
+            {
+                Console.WriteLine("Erro");
+            }
+
+        }
+
+        public void desempilhar()
+        {
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Pilha vazia");
+            }
+
+            lista.RemoveAt(lista.Count - 1); 
+
+        }
+
+        public void listar()
+        {
+            Console.WriteLine("Método listar iniciado");
+            for(int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine($"chave:{lista[i]}");
+            }
+            Console.WriteLine("Método listar finalizado");
+        }
+
+
+    }
+}
---
+++ TADPilha2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjetoPilha2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TADPilha pilha = new TADPilha(20, "pilha",30);
+            pilha.empilhar(5);
+            pilha.empilhar(4);
+            pilha.empilhar(8);
+            pilha.empilhar(1);
+            pilha.desempilhar();
+            pilha.empilhar(3);
+            pilha.listar();
+            pilha.empilhar(20);
+            pilha.empilhar(5);
+            pilha.listar();
+
+        }
+    }
+}
